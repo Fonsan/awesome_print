26af24593456652d7276c8f1847f3d42fb3f9ace
Erik Fonsan Fonselius <fonsan@gmail.com>
2010-07-13 10:40:55 +0200

    When fixing previous bug changes to the code did not consider forcing recursive level to 1

--- lib/ap/mixin/active_record.rb
+++ lib/ap/mixin/active_record.rb
@@ -49,9 +49,13 @@ module AwesomePrintActiveRecord
       res = object.send(r.name)
       if res.is_a?(Array)
         if res.length > 5 #@base.options[:ar_collection_limit]
-          res = res[0..5].map do |record|
-            awesome_active_record_instance_to_hash(record)
-          end + ["Only printing first five records out of (#{res.length}) records"]
+          res = res[0..5]
+        end
+        res.map! do |record|
+          awesome_active_record_instance_to_hash(record)
+        end
+        if res.length > 5
+          res += ["Only printing first five records out of (#{res.length}) records"]
         end
       else
         res =  awesome_active_record_instance_to_hash(res)
